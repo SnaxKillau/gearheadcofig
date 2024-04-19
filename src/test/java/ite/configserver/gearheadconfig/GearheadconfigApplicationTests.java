3a3d3926bf1e86aff4ca13777ba377ce305f9091
Versna <phopversna007@gmail.com>
2024-04-19 19:14:05 +0700

    first config commit

---
+++ src/test/java/ite/configserver/gearheadconfig/GearheadconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package ite.configserver.gearheadconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearheadconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
